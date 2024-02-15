06adc276e134ef65716b9f88f6447a0aa3054549
Cesar1308 <cesarpereira.senac@#gmail.com>
2024-02-15 14:45:32 -0300

    feat: inset prettier and eslint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", function(req, res){
   res.send("Hello World")
 });
-
+ 
 app.get("/user/:nome/sobrenome/:sobrenome", function(req, res){
   res.send({
     nome: req.params.nome,
