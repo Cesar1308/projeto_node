d48c38a982a3f2e5255b4349fda7d8da9afe7d2e
Cesar1308 <cesarpereira.senac@#gmail.com>
2024-02-15 11:16:11 -0300

    feat: insert params in url

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ app.get("/", function(req, res){
   res.send("Hello World")
 });
 
-app.get("/user", function(req, res){
+app.get("/user/:nome/sobrenome/:sobrenome", function(req, res){
   res.send({
-    nome: "César"
-  })
-});
-
-app.delete("/user", function(req, res){
-  res.send({
-    nome: "César Deletado"
+    nome: req.params.nome,
+    sobrenome: req.params.sobrenome
   })
 });
 
